cd8e04dd183fec0e93305504e2241f9398cfd6e8
elise <hei@elisejakob.no>
2025-04-28 18:06:01 +0200

    add some pixel life, edit color variables

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -3,8 +3,21 @@ import styles from "./footer.module.scss";
 
 export const Footer = () => {
   return (
-    <footer id="contact" className={styles.footer}>
-      <p>Contact goes here</p>
+    <footer className={styles.footer}>
+      <div className={styles.skyline}>
+        <div className={styles.clouds}></div>
+        <div className={styles.trees}></div>
+      </div>
+      <section className={styles.related}>
+        <h2>Want to keep reading?</h2>
+        <div className={styles.house}>
+        <img src="/assets/pixels/houses/big-house.png" />
+        </div>
+      </section>
+      <section id="contact" className={styles.contact}>
+        <h2>About Alexandra</h2>
+        <div className={styles.monster}></div>
+      </section>
     </footer>
   );
 };

e5d3b2386ef6eb92ff06b2dd38f845bb0195c234
elise <hei@elisejakob.no>
2025-05-21 23:41:38 +0200

    contact info in footer + some minor adjustments

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -12,11 +12,27 @@ export const Footer = () => {
           <span>About Alexandra</span>
         </h1>
         <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
-        <div className={styles.monster}>
-          <Sprite width="135px" height="96px" frames="8" speed="1s" url="url('/assets/pixels/monsters/forestmonster/Bush_Monster-Idle.png')" />
-          <Sprite width="135px" height="96px" frames="18" speed="1s" url="url('/assets/pixels/monsters/forestmonster/Bush_Monster-Attack.png')" />
-        </div>
+        <h2 className="iconHeading">
+          <Icon type="contact" />
+          <span>Get in touch</span>
+        </h2>
+        <ul className={styles.contactItems}>
+          <li className={styles.email}>
+            <span>Email</span>
+            <a href="mailto:hallo@troubalex.de" target="_blank">hallo@troubalex.de</a>
+          </li>
+          <li>
+            <a href="https://linkedin.com/troubalex" target="_blank">LinkedIn</a>
+          </li>
+          <li>
+            <a href="https://github.com/troubalex" target="_blank">GitHub</a>
+          </li>
+        </ul>
       </section>
+      <div className={styles.monster}>
+        <Sprite width="135px" height="96px" frames="8" speed="1s" url="url('/assets/pixels/monsters/forestmonster/Bush_Monster-Idle.png')" />
+        <Sprite width="135px" height="96px" frames="18" speed="1s" url="url('/assets/pixels/monsters/forestmonster/Bush_Monster-Attack.png')" />
+      </div>
     </footer>
   );
 };
